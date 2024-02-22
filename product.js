e048606461e139fad4566646e30a4de5cb445dc4
Michael Musya <149810874+iammoneymic@users.noreply.github.com>
2024-02-22 16:21:35 -0500

    Update product.js

--- product.js
+++ product.js
@@ -1,19 +1,34 @@
-// src/models/Product.js
-const mongoose = require('mongoose');
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Product from './Product';
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  description: String,
-  imageUrl: String
-});
 
-const Product = mongoose.model('Product', productSchema);
+function Products() {
+  const [products, setProducts] = useState([]);
 
-module.exports = Product;
+  useEffect(() => {
+    
+    axios.get('http://localhost:5000/api/products')
+      .then(response => {
+        setProducts(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching products:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Products</h2>
+      <ul>
+        {products.map(product => (
+          <li key={product._id}>
+            <Product product={product} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Products;
